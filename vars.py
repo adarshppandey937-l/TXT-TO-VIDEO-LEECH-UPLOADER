f5921aa6e7206605a3c2c478f89d7fa2aa55115f
adarshppandey937-l <adarshppandey937@gmail.com>
2025-12-23 22:37:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,22 +2,23 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23916386"))
-API_HASH = environ.get("API_HASH", "613666377f6a46bba98777abf5ce402e")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8249024505:AAHW-sQPzCw9nCCWeYHm4qSNQh4b5K7XGsQ")
+API_ID = int(environ.get("API_ID", "22865155"))
+API_HASH = environ.get("API_HASH", "e430e3f61712616b926be959f1612c46")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8203333831:AAE1c3ip57X_xKzoiJRQ5N8WVGaGKstl-aM")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "neerajdrm)  # Channel username without @, 
 FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/neerajdrm")  # Channel link
 
 # Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "1483382098").split()))
+ADMINS = list(map(int, environ.get("ADMINS", "8453406690").split()))
 
 # Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", "1483382098"))
+OWNER_ID = int(environ.get("OWNER_ID", "8453406690"))
 
 # Database URL (if you want to add database support later)
-DATABASE_URL = environ.get("DATABASE_URL", "")
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://adarshppandey937:uIoPcln9vXQBF0vP@cluster0.o9mn6hb.mongodb.net/?")
+
 
 
 
