cf53e2b1eb2b391b0fb65bcaf2568b39389b8a01
adarshppandey937-l <adarshppandey937@gmail.com>
2026-02-19 22:44:29 +0530

    Remove extra blank lines in vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_HASH = environ.get("API_HASH", "e430e3f61712616b926be959f1612c46")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8203333831:AAE1c3ip57X_xKzoiJRQ5N8WVGaGKstl-aM")
 
 # Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "neerajdrm)  # Channel username without @, 
+FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "neerajdrm")  # Channel username without @, 
 FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/neerajdrm")  # Channel link
 
 # Admin Configuration
@@ -18,6 +18,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "8453406690"))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://adarshppandey937:uIoPcln9vXQBF0vP@cluster0.o9mn6hb.mongodb.net/?")
+
 
 
 
